56289b3ff16baefe18543877f4242512ed10949f
luoy <luoy@emnets.org>
2017-05-31 10:34:18 +0800

    add binay path detection and responding tests

--- src/main/java/io/appetizerio/ReplayKit.java
+++ src/main/java/io/appetizerio/ReplayKit.java
@@ -1,21 +1,41 @@
 package io.appetizerio;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by luoy on 2017/5/31.
  */
 public class ReplayKit {
-    public ReplayKit() {
-        mADBHost = "127.0.01";
-        mADBPort = 5037;
-    }
-    public ReplayKit(String host, int port) {
-        mADBHost = host;
-        mADBPort = port;
+    public ReplayKit(String binBasePath) throws FileNotFoundException {
+        this("127.0.01", 5037, binBasePath);
     }
 
-    public void showDebugMessage() {
-        System.out.println(String.format("Host: %s, Port: %d", mADBHost, mADBPort));
+    public ReplayKit(String host, int port, String binBasePath) throws FileNotFoundException {
+        mADBHost = host;
+        mADBPort = port;
+        String targetBinName = null;
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("mac") || osName.contains("darwin")) {
+            targetBinName = "appetizer";
+        } else if (osName.contains("win")) {
+            targetBinName = "appetizer.exe";
+        } else if (osName.contains("nux")) {
+            targetBinName = "appetizer";
+        } else {
+            throw new RuntimeException("Unsupported platform");
+        }
+        File targetFile = new File(binBasePath, targetBinName);
+        if (targetFile.exists() && !targetFile.isDirectory()) {
+            mBinPath = Paths.get(binBasePath, targetBinName);
+        } else {
+            throw new FileNotFoundException();
+        }
     }
+
     private String mADBHost;
+    private Path mBinPath;
     private int mADBPort;
 }
--- src/test/java/io/appetizerio/ReplayKitTest.java
+++ src/test/java/io/appetizerio/ReplayKitTest.java
@@ -2,14 +2,28 @@ package io.appetizerio;
 
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
 
 /**
  * Created by luoy on 2017/5/31.
  */
 public class ReplayKitTest {
     @Test
-    public void testShowDebugMessage() {
-        ReplayKit kit = new ReplayKit("127.0.0.1", 4444);
-        kit.showDebugMessage();
+    public void testConstructor() {
+        // invalid path
+        try {
+            ReplayKit kit = new ReplayKit("/code/");
+            fail();
+        } catch (FileNotFoundException e) {
+        }
+
+        // valid path for my machine
+        try {
+            ReplayKit kit = new ReplayKit("D:\\code\\appetizer\\appetizer-toolkit\\win32");
+        } catch (FileNotFoundException e) {
+            fail();
+        }
     }
 }
